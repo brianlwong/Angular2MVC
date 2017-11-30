7f295382436c2fe0d653dea2bb34b229b3709e93
Wong, Brian <Brian.Wong@oncor.com>
2017-11-30 09:14:09 -0600

    initial commit

---
+++ Angular2MVC/app/app.module.ts
@@ -0,0 +1,25 @@
+﻿import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { routing } from './app.routing';
+
+import { HomeComponent } from './components/home.component';
+import { AppComponent } from './app.component';
+import { UserComponent } from './components/user.component';
+
+import { UserService } from './service/user.service';
+
+@NgModule({
+    //add your modules here
+    imports: [ BrowserModule, ReactiveFormsModule, HttpModule, routing ],
+    //add your components here
+    declarations: [ AppComponent, HomeComponent, UserComponent ],
+    //add your services here
+    providers: [ UserService, { provide: APP_BASE_HREF, useValue: '/' } ],
+    bootstrap: [ AppComponent ]
+})
+
+export class AppModule { }
